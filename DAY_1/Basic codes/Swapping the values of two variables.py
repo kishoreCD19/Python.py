36f963b7e0e66443b6852af002a46af64d7a9757
kishore c d <124656453+kishoreCD19@users.noreply.github.com>
2023-02-20 21:30:11 +0530

    Create Swapping the values of two variables.py

---
+++ DAY_1/Basic codes/Swapping the values of two variables.py
@@ -0,0 +1,9 @@
+#SWAPPING THE VALUES OF TWO NUMBERS USING PYTHON
+k = 10
+i = 20
+print('Before swapping k is',k,'I is ',i)
+Before swapping k is 10 I is  20
+k,i=i,k
+print('after swapping k is',k,'I is',i)
+after swapping k is 20 I is 10
+ 
