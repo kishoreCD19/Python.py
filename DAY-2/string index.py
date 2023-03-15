a830ebb3c47f5dc598f633408986749af0f1781b
kishore c d <124656453+kishoreCD19@users.noreply.github.com>
2023-03-15 21:28:13 +0530

    Create string index.py

---
+++ DAY-2/string index.py
@@ -0,0 +1,15 @@
+print("$$$ Strinng index $$$")
+// output= $$$ String index $$$ //
+word= 'python'
+word[0]
+// output='p'//
+word[1]
+//output='y'//
+word[2]
+//output='t'//
+word[3]
+//output='h'//
+word[4]
+//output='o'//
+word[5]
+//output='n'//
