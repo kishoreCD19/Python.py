7727932aabd806bf48476413eddc35c7ad160f98
kishore c d <124656453+kishoreCD19@users.noreply.github.com>
2023-03-21 22:13:06 +0530

    Create Default parameter.py

---
+++ DAY-2/Default parameter.py
@@ -0,0 +1,11 @@
+def my_function (country = "INDIA"):
+	print("iam from" + country)
+
+	
+my_function("america")
+//output = iam fromamerica//
+my_function()
+//output = iam fromINDIA// //*default output*//
+my_function("russia")
+//output = iam fromrussia//
+ 
