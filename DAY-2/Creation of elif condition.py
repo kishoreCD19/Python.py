935e9e3888d7e5242deb586c1264950b55657dfe
kishore c d <124656453+kishoreCD19@users.noreply.github.com>
2023-04-26 21:14:25 +0530

    Create Creation of elif condition.py

---
+++ DAY-2/Creation of elif condition.py
@@ -0,0 +1,11 @@
+# print "1" if a is equal to b,print"2" if a is greater than b,oherwise print "3"
+a=100
+b=10
+if a == b:
+    print("1")
+elif a > b:
+        print("2")
+else:
+            print("3")
+
+    // output = 2 //
