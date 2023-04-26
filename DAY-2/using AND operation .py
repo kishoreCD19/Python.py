107b2f0caae80b945ac585f32f3253bc57e72f06
kishore c d <124656453+kishoreCD19@users.noreply.github.com>
2023-04-26 21:22:59 +0530

    Create using AND operation .py

---
+++ DAY-2/using AND operation .py
@@ -0,0 +1,11 @@
+# using and operation 
+#print" hello world" if a is equal to b ,and c is equal to d.
+a=19
+b=19
+c=19
+d=19
+if a == b and c == d:
+    print("hello world")
+    
+    
+    // output = hello world //
