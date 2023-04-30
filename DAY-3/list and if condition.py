6b44b2af3c0a5a3e557688a9be11b7a685aff34c
kishore c d <124656453+kishoreCD19@users.noreply.github.com>
2023-04-30 21:39:18 +0530

    Create list and if condition.py

---
+++ DAY-3/list and if condition.py
@@ -0,0 +1,10 @@
+a = ['IAM - IRON MAN', 'IAM - VENGENCE', 'IAM - KNIGHT', 'IAM  - BATMAN', 'IAM THE GOAT']
+for i in range(len(a)):
+    print(i, a[i])
+
+    
+    // output = 0 IAM - IRON MAN
+     1 IAM - VENGENCE
+     2 IAM - KNIGHT
+     3 IAM  - BATMAN
+     4 IAM THE GOAT //
