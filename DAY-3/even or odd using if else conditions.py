5e67a21c50e0d5d8516bbc17f32766e8a9f4c1c4
kishore c d <124656453+kishoreCD19@users.noreply.github.com>
2023-05-01 21:53:26 +0530

    Create even or odd using if else conditions.py

---
+++ DAY-3/even or odd using if else conditions.py
@@ -0,0 +1,15 @@
+for num in range(2, 10):
+    if num % 2 == 0:
+        print("Found an even number", num)
+        continue
+    print("Found an odd number", num)
+ 
+
+// output = Found an even number 2
+            Found an odd number 3
+            Found an even number 4
+            Found an odd number 5
+            Found an even number 6
+            Found an odd number 7
+            Found an even number 8
+            Found an odd number 9 //
