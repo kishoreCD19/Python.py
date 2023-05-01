79c33ed1641ba598a356fdb74224a80e26338209
kishore c d <124656453+kishoreCD19@users.noreply.github.com>
2023-05-01 21:50:16 +0530

    Create break and continue statement.py

---
+++ DAY-3/break and continue statement.py
@@ -0,0 +1,17 @@
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, 'equals', x, '*', n//x)
+            break
+    else:
+        # loop fell through without finding a factor
+        print(n, 'is a prime number')
+
+        // output = 2 is a prime number
+                    3 is a prime number
+                    4 equals 2 * 2
+                    5 is a prime number
+                    6 equals 2 * 3
+                    7 is a prime number
+                    8 equals 2 * 4
+                     9 equals 3 * 3 //
