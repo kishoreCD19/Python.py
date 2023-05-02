32eeed3da4948fee69e5a72a4d04d3727ac10397
kishore c d <124656453+kishoreCD19@users.noreply.github.com>
2023-05-02 22:18:05 +0530

    Create while and continue .py

---
+++ DAY-3/while and continue .py
@@ -0,0 +1,15 @@
+#using the while and continue  conditions
+i=0
+while i<6:
+    i+=1
+    if i == 3:
+        continue
+    print(i)
+    
+    
+    // output = 1
+2
+4
+5
+6 //
+# when i=3 loop will break and continued 
