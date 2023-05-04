c804b99325ab8003125177e35fe3caf05ff0f80e
kishore c d <124656453+kishoreCD19@users.noreply.github.com>
2023-05-04 10:11:46 +0530

    Create using same class with various objects.py

---
+++ DAY-3/using same class with various objects.py
@@ -0,0 +1,11 @@
+class MyClass():
+	k=["k","i","s","h","o","r","e"]
+
+  k1=MyClass()
+k2=MyClass()
+print(k1.k)
+// output = ['k', 'i', 's', 'h', 'o', 'r', 'e']//
+print(k2.k)
+// output = ['k', 'i', 's', 'h', 'o', 'r', 'e']//
+
+#class= MyClass,object = k1 & k2
