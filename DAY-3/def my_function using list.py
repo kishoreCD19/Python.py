cb4968f46c6bcbdb440d5197e6566a552064bda3
kishore c d <124656453+kishoreCD19@users.noreply.github.com>
2023-05-04 09:55:02 +0530

    Create def my_function using list.py

---
+++ DAY-3/def my_function using list.py
@@ -0,0 +1,12 @@
+kids=["ally","bob","stromer"]
+def my_function(*kids):
+    print("the youngest child is " +kids[2])
+    // output = the youngest child is stromer //
+    
+    print(" the elder child is " +kids[1])
+    
+    // output = the elder child is bob //
+    
+    print(" the first child is " +kids[0])
+    
+    // output = the first child is ally //
