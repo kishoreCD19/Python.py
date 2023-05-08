ba7a3404cce8b33123710fc21bdf375190a86f5e
kishore c d <124656453+kishoreCD19@users.noreply.github.com>
2023-05-08 21:55:29 +0530

    Create changing the class types .py

---
+++ DAY-3/changing the class types .py
@@ -0,0 +1,16 @@
+#using integer datatype to string 
+type('1')
+
+// output = str //
+
+#using integer datatype to define the class 
+
+type(1)
+
+// output = int //
+
+#using the float datatype deffine the class 
+type(19.0)
+
+// output = float //
+
